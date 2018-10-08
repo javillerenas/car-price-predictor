2f0fb987df68b4e6baf8403bf45c5a5acaf3d862
Javier Llerenas <javr.llerenas@gmail.com>
2018-10-08 15:51:45 -0700

    [app] connect the client to the predictor app

--- app_client/app.py
+++ app_client/app.py
@@ -11,10 +11,13 @@
 from flask import Flask, session, render_template, url_for, redirect
 from flask_bootstrap import Bootstrap
 from forms import CarForm
+import requests, json   # for post request to predictor
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Super extra secret key'
 bootstrap = Bootstrap(app)
+
+PREDICTOR_URL = "http://127.0.0.1:8080/api"
 
 ## ===== Data =======
 ##
@@ -40,11 +43,22 @@ def index():
     form = CarForm()
     form.make.choices      = marks 
     form.fuel_type.choices = fuel_type
-    
+
     if form.validate_on_submit():
-       car_name  = form.make.data
-       car_price = 22_000_000 # make api call
-       return render_template('priced.html', car_name=car_name, car_price=car_price)
+
+        ## make api call to predictor
+        car_make = form.make.data
+        data = json.dumps({
+            'year_model':   form.year.data, 
+            'mileage':      form.miles.data,
+            'fiscal_power': form.fiscal_pow.data, 
+            'fuel_type':    form.fuel_type.data, 
+            'mark':         car_make
+        })
+        r = requests.post(PREDICTOR_URL, data)
+        car_price = float( r.json()['price'] * 0.27 )
+        return render_template('priced.html', car_make=car_make, car_price=car_price, make='AAA')
+    
     return render_template('index.html', form=form)
 
 if __name__ == "__main__":
