745eb868fdfba7af9bf91bbd558c1213e5b8900f
Javier Llerenas <javr.llerenas@gmail.com>
2019-01-24 18:46:19 -0800

    init migration from flask to vue

---
+++ client_app/src/stories/index.stories.js
@@ -0,0 +1,32 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { storiesOf } from "@storybook/vue";
+import { action } from "@storybook/addon-actions";
+import { linkTo } from "@storybook/addon-links";
+
+import MyButton from "../components/MyButton.vue";
+import HelloWorld from "../components/HelloWorld.vue";
+
+storiesOf("Button", module)
+  .add("with text", () => ({
+    components: { MyButton },
+    template: '<my-button @click="action">Hello Button</my-button>',
+    methods: { action: action("clicked") }
+  }))
+  .add("with JSX", () => ({
+    components: { MyButton },
+    render() {
+      return <my-button onClick={this.action}>With JSX</my-button>;
+    },
+    methods: { action: linkTo("Button", "with some emoji") }
+  }))
+  .add("with some emoji", () => ({
+    components: { MyButton },
+    template: '<my-button @click="action">😀 😎 👍 💯</my-button>',
+    methods: { action: action("clicked") }
+  }));
+
+  storiesOf("HelloWorld", module)
+    .add("hello Storybook", () => ({
+      components: { HelloWorld },
+      template: '<HelloWorld />'
+    }))
